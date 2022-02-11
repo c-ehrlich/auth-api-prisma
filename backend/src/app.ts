d1ff5a2bf4512267954b4c404dcc539dc9402895
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-02-11 13:25:41 +0100

    feat(backend): Create router and healthcheck

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,11 +4,13 @@ import config from 'config';
 import connectToDb from './utils/connectToDb';
 import log from './utils/logger';
 
+import router from './routes';
+
 const app = express();
+app.use(router);
 
 const port = config.get('port');
 app.listen(port, () => {
   log.info(`App started at http://localhost:${port}`);
-
   connectToDb();
 });
