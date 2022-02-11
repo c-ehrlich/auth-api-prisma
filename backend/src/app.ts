4481c2000e49df45e9ee1e015fd3b7254837382e
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-02-11 12:53:31 +0100

    (feat)backend: Create logger

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,14 @@
-console.log("hello world");
+require('dotenv').config();
+import express from 'express';
+import config from 'config';
+import connectToDb from './utils/connectToDb';
+import log from './utils/logger';
+
+const app = express();
+
+const port = config.get('port');
+app.listen(port, () => {
+  log.info(`App started at http://localhost:${port}`);
+
+  connectToDb();
+});
